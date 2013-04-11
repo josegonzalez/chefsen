c770390b180c60ca26b6117123e44420974d1770
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-04-11 00:30:54 -0400

    fix the checksum on sequel-pro

--- cookbooks/sequel-pro/attributes/default.rb
+++ cookbooks/sequel-pro/attributes/default.rb
@@ -1,2 +1,2 @@
 node.default[:sequel_pro][:source] = "http://sequel-pro.googlecode.com/files/sequel-pro-1.0.1.dmg"
-node.default[:sequel_pro][:checksum] = "derp"
+node.default[:sequel_pro][:checksum] = "256873df2bdaebad9997acb2182428790454cb577d2c1af9565ecc4f670ebf92"
